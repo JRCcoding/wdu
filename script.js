fbe6edc01f2860ccf6fd68d0aac123384fdd1d4f
JRCcoding <87212333+JRCcoding@users.noreply.github.com>
2022-07-08 08:04:48 -0500

    reverse script for toggle

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function webDevTxt () {
     var x = document.getElementById("devTxt");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
 function digitalDesign() {
     var z = document.getElementById("desTxt");
-    if (z.style.display === "none") {
-        z.style.display = "block";
-    } else {
+    if (z.style.display === "block") {
         z.style.display = "none";
+    } else {
+        z.style.display = "block";
     }
 }
 
 function familyMan() {
     var y = document.getElementById("famMan");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    } else {
+    if (y.style.display === "block") {
         y.style.display = "none";
+    } else {
+        y.style.display = "block";
     }
 }
